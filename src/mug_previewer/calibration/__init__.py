85615042c6bea302bf12136c823658bdd924cd6c
desdodec <desdodec@gmail.com>
2026-09-20 22:36:52 +0100

    Export native calibration target API

--- src/mug_previewer/calibration/__init__.py
+++ src/mug_previewer/calibration/__init__.py
@@ -10,7 +10,14 @@ from .session import (
     save_candidate_profile,
     save_fit_session,
 )
-from .target import CalibrationTargetSpec, render_calibration_target, save_calibration_target
+from .target import (
+    CalibrationTargetSpec,
+    ProviderCalibrationTargetSpec,
+    render_calibration_target,
+    render_provider_calibration_target,
+    save_calibration_target,
+    save_provider_calibration_target,
+)
 
 __all__ = [
     "CalibrationFit",
@@ -22,6 +29,9 @@ __all__ = [
     "save_candidate_profile",
     "save_fit_session",
     "CalibrationTargetSpec",
+    "ProviderCalibrationTargetSpec",
     "render_calibration_target",
+    "render_provider_calibration_target",
     "save_calibration_target",
+    "save_provider_calibration_target",
 ]
